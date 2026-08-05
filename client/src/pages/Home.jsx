e2a1cb384b9363fc577c3a27d1801791a5ed2186
Vindhyachal Gupta <vindhyachalgupta1922@gmail.com>
2026-08-06 02:22:28 +0530

    Day 2 - Project Setup and Hero Section

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+function Home(){
+    return (
+        <section>
+            <div className="min-h-screen flex items-center justify-center">
+                <h1 className="text-center">
+                    Build Your Dream Resume
+                </h1>
+                <p className="mt-4 text-lg text-gray-600">
+                    Build professional resumes using AI in minutes. 
+                </p>
+                <button className="mt-6 bg-green-600 text-white px-6 py-3 rounded-lg">
+                    Get Started
+                </button>
+                <button> Watch Demo</button>
+            </div>
+        </section>
+    );
+}
+export default Home;
