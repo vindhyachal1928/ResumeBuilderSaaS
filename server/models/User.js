d60e510f2dbee52e2defb41188842b011d9515a9
Vindhyachal Gupta <vindhyachal1028@gmail.com>
2026-08-12 11:52:54 +0530

    Day 10 - Add password validation

--- server/models/User.js
+++ server/models/User.js
@@ -1,28 +1,39 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-            lowercase: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
+const passwordRegex =
+  /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%!]).{8,}$/;
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-        timestamps: true,
-    }
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      match: [
+        passwordRegex,
+        "Password must contain at least 8 characters, 1 uppercase, 1 lowercase, 1 number, and 1 special character (@#$%!).",
+      ],
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", userSchema);
+
 module.exports = User;
